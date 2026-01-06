986072f7c6db5ca1f08353fd1b253952e6a8a243
alex-omophub <sdk@omophub.com>
2026-01-06 15:40:47 +0000

    Tests extension

--- tests/testthat/test-hierarchy.R
+++ tests/testthat/test-hierarchy.R
@@ -161,3 +161,186 @@ test_that("hierarchy$descendants includes optional params", {
   expect_equal(called_with$query$include_invalid, "true")
   expect_equal(called_with$query$domain_ids, "Condition")
 })
+
+# ==============================================================================
+# get() method
+# ==============================================================================
+
+test_that("hierarchy$get validates concept_id", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  expect_error(resource$get("invalid"))
+  expect_error(resource$get(-1))
+  expect_error(resource$get(0))
+})
+
+test_that("hierarchy$get calls correct endpoint", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(path = path, query = query)
+      list(ancestors = list(), descendants = list())
+    }
+  )
+
+  resource$get(201826)
+
+  expect_equal(called_with$path, "concepts/201826/hierarchy")
+})
+
+test_that("hierarchy$get includes format parameter", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, format = "graph")
+
+  expect_equal(called_with$query$format, "graph")
+})
+
+test_that("hierarchy$get defaults format to flat", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826)
+
+  expect_equal(called_with$query$format, "flat")
+})
+
+test_that("hierarchy$get joins vocabulary_ids correctly", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, vocabulary_ids = c("SNOMED", "ICD10CM"))
+
+  expect_equal(called_with$query$vocabulary_ids, "SNOMED,ICD10CM")
+})
+
+test_that("hierarchy$get joins domain_ids correctly", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, domain_ids = c("Condition", "Drug"))
+
+  expect_equal(called_with$query$domain_ids, "Condition,Drug")
+})
+
+test_that("hierarchy$get caps max_levels at 20", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, max_levels = 50)  # Request 50
+
+  expect_equal(called_with$query$max_levels, 20L)  # Capped at 20
+})
+
+test_that("hierarchy$get includes max_results parameter", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, max_results = 100)
+
+  expect_equal(called_with$query$max_results, 100L)
+})
+
+test_that("hierarchy$get joins relationship_types correctly", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, relationship_types = c("Is a", "Subsumes"))
+
+  expect_equal(called_with$query$relationship_types, "Is a,Subsumes")
+})
+
+test_that("hierarchy$get adds include_invalid when TRUE", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, include_invalid = TRUE)
+
+  expect_equal(called_with$query$include_invalid, "true")
+})
+
+test_that("hierarchy$get does not add include_invalid when FALSE", {
+  base_req <- httr2::request("https://api.omophub.com/v1")
+  resource <- HierarchyResource$new(base_req)
+
+  called_with <- NULL
+  local_mocked_bindings(
+    perform_get = function(req, path, query = NULL) {
+      called_with <<- list(query = query)
+      list()
+    }
+  )
+
+  resource$get(201826, include_invalid = FALSE)
+
+  expect_null(called_with$query$include_invalid)
+})
